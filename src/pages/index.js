ddb1b06ff0bba077ceeb62132867fe7ecd81fd56
Fernando Serpa <serpaef@outlook.com>
2022-03-12 11:36:00 -0300

    feat: Exporta páginas da aplicação

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Dashboard from './Dashboard';
+import Goals from './Goals';
+import Transactions from './Transactions';
+import Config from './Config';
+import NotFound from './NotFound';
+
+export {
+  Dashboard,
+  Goals,
+  Transactions,
+  Config,
+  NotFound
+};
