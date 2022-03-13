e1599dede15bf34405f3c558cf347b7f040d5c4c
Fernando Serpa <serpaef@outlook.com>
2022-03-13 18:23:40 -0300

    feat: Cria arquivo de exportação de componentes

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Accounts from './Accounts';
+import ExpensesByCategory from './ExpensesByCategory';
+import Goals from './Goals';
+import Header from './Header';
+import Overview from './Overview';
+
+export {
+  Accounts,
+  ExpensesByCategory,
+  Goals,
+  Header,
+  Overview,
+};
