3a57dd38c9c9c74dff9c44bdd23c891bf16bc9f5
Fernando Serpa <serpaef@outlook.com>
2022-03-13 16:52:42 -0300

    feat: implementa card de gastos por categoria

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col, Container, Card } from 'react-bootstrap';
+import ExpensesByCategory from '../components/ExpensesByCategory';
 import Overview from '../components/Overview';
 
 export default function Dashboard() {
@@ -13,7 +14,7 @@ export default function Dashboard() {
         </Row>
         <Row>
           <Col md='5' style={{ marginBottom: '0.5rem' }}>
-            <Card border='secondary' style={{ minHeight: '35vh' }}>
+            <Card border='secondary' style={{ height: '35vh' }}>
               <Card.Header>Visão Geral</Card.Header>
               <Card.Body>
                 <Overview />
@@ -21,21 +22,17 @@ export default function Dashboard() {
             </Card>
           </Col>
           <Col md='7' style={{ marginBottom: '0.5rem' }}>
-            <Card border='secondary' style={{ minHeight: '35vh' }}>
+            <Card border='secondary' style={{ height: '35vh' }}>
               <Card.Header>Despesas por categoria</Card.Header>
               <Card.Body>
-                <Card.Title>Secondary Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
+                <ExpensesByCategory />
               </Card.Body>
             </Card>
           </Col>
         </Row>
         <Row>
           <Col md='7' style={{ marginBottom: '0.5rem' }}>
-            <Card border='secondary' style={{ minHeight: '35vh' }}>
+            <Card border='secondary' style={{ height: '35vh' }}>
               <Card.Header>Metas</Card.Header>
               <Card.Body>
                 <Card.Title>Secondary Card Title</Card.Title>
@@ -47,7 +44,7 @@ export default function Dashboard() {
             </Card>
           </Col>
           <Col md='5' style={{ marginBottom: '0.5rem' }}>
-            <Card border='secondary' style={{ minHeight: '35vh' }}>
+            <Card border='secondary' style={{ height: '35vh' }}>
               <Card.Header>Suas Contas</Card.Header>
               <Card.Body>
                 <Card.Title>Secondary Card Title</Card.Title>
