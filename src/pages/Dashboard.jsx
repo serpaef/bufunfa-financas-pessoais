0401daebff05de6543ea6f3294f8e43c98aa1803
Fernando Serpa <serpaef@outlook.com>
2022-03-12 21:53:54 -0300

    feat: Cria setup inicial de layout do dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,68 @@
 import React from 'react';
+import { Row, Col, Container, Card } from 'react-bootstrap';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col style={{ marginBottom: '1rem' }}>
+            <h2>Dashboard</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col md='5' style={{ marginBottom: '0.5rem' }}>
+            <Card border='secondary' style={{ minHeight: '35vh' }}>
+              <Card.Header>Visão Geral</Card.Header>
+              <Card.Body>
+                <Card.Title>Secondary Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md='7' style={{ marginBottom: '0.5rem' }}>
+            <Card border='secondary' style={{ minHeight: '35vh' }}>
+              <Card.Header>Despesas por categoria</Card.Header>
+              <Card.Body>
+                <Card.Title>Secondary Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col md='7' style={{ marginBottom: '0.5rem' }}>
+            <Card border='secondary' style={{ minHeight: '35vh' }}>
+              <Card.Header>Metas</Card.Header>
+              <Card.Body>
+                <Card.Title>Secondary Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md='5' style={{ marginBottom: '0.5rem' }}>
+            <Card border='secondary' style={{ minHeight: '35vh' }}>
+              <Card.Header>Suas Contas</Card.Header>
+              <Card.Body>
+                <Card.Title>Secondary Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
