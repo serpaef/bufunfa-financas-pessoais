1e1cf7a1cc33a00c5fcbd1924dc45cddecaee4cf
Fernando Serpa <serpaef@outlook.com>
2022-03-14 12:30:36 -0300

    feat: encapsula aplicação dentro do contexto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Context from './context';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Context>
+      <App />
+    </Context>
   </React.StrictMode>,
   document.getElementById('root')
 );
