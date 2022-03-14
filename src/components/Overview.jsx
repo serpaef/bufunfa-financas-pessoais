82bb104fe823b6be37f9bcaf8b7110e2a822b5f0
Fernando Serpa <serpaef@outlook.com>
2022-03-14 13:36:11 -0300

    feat: saldo total de contas no widget Overview

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Overview.css'
 
+import { useAccounts } from '../context/Context';
+
 export default function Overview() {
+  const [totalBalance, setTotalBalance] = useState(0);
+  const { accounts } = useAccounts();
+
+  useEffect(() => {
+    if (accounts.length > 0) {
+      const total = accounts.reduce((acc, curr) => acc + curr.balance, 0)
+      setTotalBalance(+total);
+    }
+  }, [accounts]);
+
   return (
     <div className='overview-in'>
       <ul className='overview-list'>
         <li className='overview-item'>
           <span>Saldo Total</span>
-          <span>R$48,00</span>
+          <span>R${totalBalance.toFixed(2)}</span>
         </li>
         <li className='overview-item'>
           <span>Receitas</span>
