0162957bef5b6da4f24638cb0e1822bd599629fc
Fernando Serpa <serpaef@outlook.com>
2022-03-14 13:57:35 -0300

    feat: implementa lógica de transações no widget overview

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,18 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import './Overview.css'
 
-import { useAccounts } from '../context/Context';
+import { useAccounts, useTransactions } from '../context/Context';
+
+const RECEIPT = 1;
+const EXPENSE = 2;
 
 export default function Overview() {
   const [totalBalance, setTotalBalance] = useState(0);
+  const [totalReceipt, setTotalReceipt] = useState(0);
+  const [totalExpenses, setTotalExpenses] = useState(0);
+
   const { accounts } = useAccounts();
+  const { transactions } = useTransactions();
 
   useEffect(() => {
     if (accounts.length > 0) {
-      const total = accounts.reduce((acc, curr) => acc + curr.balance, 0)
-      setTotalBalance(+total);
+      const total = accounts.reduce((acc, curr) => acc + curr.balance, 0);
+      setTotalBalance(total);
     }
   }, [accounts]);
+
+  useEffect(() => {
+    if(transactions.length > 0) {
+      const receipt = transactions.reduce((acc, curr) => {
+        if(curr.transactionType.id === RECEIPT) {
+          return acc + curr.value;
+        }
+        return acc;
+      }, 0);
+
+      const expense = transactions.reduce((acc, curr) => {
+        if(curr.transactionType.id === EXPENSE) {
+          return acc + curr.value;
+        }
+        return acc;
+      }, 0);
+
+      setTotalReceipt(receipt);
+      setTotalExpenses(expense);
+    }
+  }, [transactions]);
 
   return (
     <div className='overview-in'>
@@ -23,11 +51,11 @@ export default function Overview() {
         </li>
         <li className='overview-item'>
           <span>Receitas</span>
-          <span>R$100,00</span>
+          <span>R${totalReceipt.toFixed(2)}</span>
         </li>
         <li className='overview-item'>
           <span>Despesas</span>
-          <span>R$52,00</span>
+          <span>R${totalExpenses.toFixed(2)}</span>
         </li>
       </ul>
     </div>
