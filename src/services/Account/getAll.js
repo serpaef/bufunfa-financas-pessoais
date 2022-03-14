17570b5620b5da27e6f280f729e681382d513843
Fernando Serpa <serpaef@outlook.com>
2022-03-14 12:27:56 -0300

    fix: conserta retorno da função getAll

--- src/services/Account/getAll.js
+++ src/services/Account/getAll.js
@@ -3,7 +3,7 @@ import api, { headers } from '../api';
 export default async function getAll() {
   try {
     const accounts = await api.get('/accounts', headers)
-    return accounts;
+    return accounts.data;
   } catch (e) {
     return Promise.resolve([]);
   }
