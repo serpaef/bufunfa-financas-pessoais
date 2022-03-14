a4bdb60912b05f30a12c6c842e798d02fb7b5eb0
Fernando Serpa <serpaef@outlook.com>
2022-03-14 13:48:21 -0300

    feat: seta transações no Store

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,24 +1,28 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import Account from '../services/Account';
+import Transaction from '../services/Transaction'
 
 const Store = createContext();
 
 export default function Context({ children }) {
   const [accounts, setAccounts] = useState([]);
+  const [transactions, setTransactions] = useState([])
 
   async function populate() {
     const apiAccounts = await Account.getAll();
+    const apiTransactions = await Transaction.getAll();
     setAccounts(apiAccounts);
+    setTransactions(apiTransactions);
   }
 
   useEffect(() => {
     populate();
   }, []);
 
-  useEffect(() => {}, [accounts]);
+  useEffect(() => {}, [accounts, transactions]);
 
   return (
-    <Store.Provider value={{ accounts }}>
+    <Store.Provider value={{ accounts, transactions }}>
       {children}
     </Store.Provider>
   );
@@ -27,4 +31,9 @@ export default function Context({ children }) {
 export function useAccounts() {
   const { accounts } = useContext(Store);
   return { accounts };
+}
+
+export function useTransactions() {
+  const { transactions } = useContext(Store);
+  return { transactions };
 }
