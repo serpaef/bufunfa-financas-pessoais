e99009d8f976040b14bd4c5cfe0b3439b46759ab
Fernando Serpa <serpaef@outlook.com>
2022-03-15 08:02:24 -0300

    feat: Adiciona goals ao context

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,18 +1,23 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import Account from '../services/Account';
 import Transaction from '../services/Transaction'
+import Goal from '../services/Goal';
 
 const Store = createContext();
 
 export default function Context({ children }) {
   const [accounts, setAccounts] = useState([]);
-  const [transactions, setTransactions] = useState([])
+  const [transactions, setTransactions] = useState([]);
+  const [goals, setGoals] = useState([]);
 
   async function populate() {
     const apiAccounts = await Account.getAll();
     const apiTransactions = await Transaction.getAll();
+    const apiGoals = await Goal.getAll();
+
     setAccounts(apiAccounts);
     setTransactions(apiTransactions);
+    setGoals(apiGoals)
   }
 
   useEffect(() => {
@@ -22,7 +27,7 @@ export default function Context({ children }) {
   useEffect(() => {}, [accounts, transactions]);
 
   return (
-    <Store.Provider value={{ accounts, transactions }}>
+    <Store.Provider value={{ accounts, transactions, goals }}>
       {children}
     </Store.Provider>
   );
@@ -36,4 +41,9 @@ export function useAccounts() {
 export function useTransactions() {
   const { transactions } = useContext(Store);
   return { transactions };
+}
+
+export function useGoals() {
+  const { goals } = useContext(Store);
+  return { goals };
 }
